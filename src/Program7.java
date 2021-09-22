8d3c7cbbf4c2adca315023bdb97ec17129315f26
Tarun <tkumar31@gmail.com>
2021-09-22 22:25:25 +0530

    third commit

--- src/Program7.java
+++ src/Program7.java
@@ -4,6 +4,18 @@ public class Program7 {
 	
 	public static void main(String[] args) {
 		
+		String someString = "elephanteeee";
+		char someChar = 'e';
+		int count = 0;
+		 
+		for (int i = 0; i < someString.length(); i++) {
+		    if (someString.charAt(i) == someChar) {
+		        count++;
+		    }
+		}
+		
+		System.out.println(count);
+		
 		
 		
 	}
